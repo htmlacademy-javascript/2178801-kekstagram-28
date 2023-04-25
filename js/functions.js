3b26de9c0f5d3b03d2428522185c4451fdb4a9cf
Evgeniy Shcherbachenko <e.shcherbachenko@icloud.com>
2023-04-25 16:14:44 +0300

    Нужно больше функций v2

--- js/functions.js
+++ js/functions.js
@@ -1,42 +1,41 @@
 // Функция для проверки длины строки //
-const lenghtCompare = function (str, maxlong) {
-  str = String(str);
-  return (str.length <= maxlong) ? true : false
+const checkLength = function (str, maxLength) {
+  return str.length <= maxLength;
 };
 
 
 //Функция для проверки, является ли строка палиндромом //
-const palindromeCheck = function (str) {
-  let hf = (Math.floor(str.length/2));
-  for (let i = 0; i < hf; i++){
-    if (str[i] !== str[str.length-1-i]){
+const checkPalindrome = function (str) {
+  const residue = (Math.floor(str.length / 2));
+  for (let i = 0; i < residue; i++){
+    if (str[i] !== str[str.length - 1 - i]){
       return false;
-    };
-  }; return true;
+    }
+  } return true;
 };
 
 /* Функция, которая принимает строку,
 извлекает содержащиеся в ней цифры от 0 до 9 и возвращает их в виде целого положительного числа */
-const getNumber = function (str) {
+const getIntegerNumber = function (str) {
   str = str.replaceAll(' ', '');
   let numbString = '';
-  for (let i = 0; i <= str.length-1; i++){
-      if (isNaN(Number(str[i])) != true ){
-          numbString = numbString + str[i];
-      };
-  };
-  if (numbString == ''){
-      return NaN
+  for (let i = 0; i <= str.length - 1; i++){
+    if (isNaN(Number(str[i])) !== true){
+      numbString = numbString + str[i];
+    }
+  }
+  if (numbString === ''){
+    return NaN;
   } else {
-     return numbString;
-  };
+    return numbString;
+  }
 };
 
 
 /*Функция, которая принимает три параметра: исходную строку,
 минимальную длину и строку с добавочными символами — и возвращает исходную строку,
 дополненную указанными символами до заданной длины.  */
-const totalStrings = function (str,long,addStr){
-  let totalStr = str.padStart(long, addStr);
+const getTotalStrings = function (str,minLength,addStr){
+  const totalStr = str.padStart(minLength, addStr);
   return totalStr;
 };
