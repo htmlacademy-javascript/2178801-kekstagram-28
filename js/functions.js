46acdd9886d339db97984eb024852bcade3cbdb9
Evgeniy Shcherbachenko <e.shcherbachenko@icloud.com>
2023-04-26 15:10:59 +0300

    Нужно больше функций v3

--- js/functions.js
+++ js/functions.js
@@ -2,40 +2,36 @@
 const checkLength = function (str, maxLength) {
   return str.length <= maxLength;
 };
-
+checkLength ();
 
 //Функция для проверки, является ли строка палиндромом //
 const checkPalindrome = function (str) {
-  const residue = (Math.floor(str.length / 2));
+  const residue = Math.floor(str.length / 2);
   for (let i = 0; i < residue; i++){
     if (str[i] !== str[str.length - 1 - i]){
       return false;
     }
   } return true;
 };
+checkPalindrome();
 
 /* Функция, которая принимает строку,
 извлекает содержащиеся в ней цифры от 0 до 9 и возвращает их в виде целого положительного числа */
-const getIntegerNumber = function (str) {
-  str = str.replaceAll(' ', '');
-  let numbString = '';
+const getIntegersFromString = function (str) {
+  let integers = '';
   for (let i = 0; i <= str.length - 1; i++){
-    if (isNaN(Number(str[i])) !== true){
-      numbString = numbString + str[i];
+    if (!isNaN(Number(str[i]))){
+      integers += str[i];
     }
   }
-  if (numbString === ''){
-    return NaN;
-  } else {
-    return numbString;
-  }
+  return integers === '' ? NaN : integers;
 };
-
+getIntegersFromString ();
 
 /*Функция, которая принимает три параметра: исходную строку,
 минимальную длину и строку с добавочными символами — и возвращает исходную строку,
 дополненную указанными символами до заданной длины.  */
-const getTotalStrings = function (str,minLength,addStr){
-  const totalStr = str.padStart(minLength, addStr);
-  return totalStr;
+const extendString = function (str,minLength,addStr){
+  return str.padStart(minLength, addStr);
 };
+extendString();
